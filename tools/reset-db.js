e4bf7890da59df229f2fa27352584e5ce4671f25
Tushar Goyal <tushar462024@gmail.com>
2021-01-20 19:25:28 +0530

    Create Reset DB node fn

--- tools/reset-db.js
+++ tools/reset-db.js
@@ -12,6 +12,7 @@ configInit()
 // Get the default connection
 const { mongoose } = db
 const conn = mongoose.connection
+const User = db.user
 const Role = db.role
 
 conn.on('connecting', function () {
@@ -24,62 +25,97 @@ conn.on('connected', function () {
 // Set up default mongoose connection to MongoDB Atlas
 mongoose.connect(config.dbURL, config.dbOptions)
 
-Role.deleteMany({}, (err) => {
-  if (err) {
-    console.log('Unable to delete roles!')
-    return
-  }
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: 'SUPERADMIN'
-      }).save(err => {
-        if (err) {
-          console.log('error', err)
-        }
+async function resetDBFn () {
+  console.log('Reset database function executed!')
 
-        console.log("added 'SUPERADMIN' to roles collection")
-      })
-
-      new Role({
-        name: 'ADMIN'
-      }).save(err => {
-        if (err) {
-          console.log('error', err)
-        }
-
-        console.log("added 'ADMIN' to roles collection")
-      })
-
-      new Role({
-        name: 'USER'
-      }).save(err => {
-        if (err) {
-          console.log('error', err)
-        }
-
-        console.log("added 'USER' to roles collection")
-      })
-
-      new Role({
-        name: 'MODERATOR'
-      }).save(err => {
-        if (err) {
-          console.log('error', err)
-        }
-
-        console.log("added 'MODERATOR' to roles collection")
-      })
-
-      new Role({
-        name: 'STAFF'
-      }).save(err => {
-        if (err) {
-          console.log('error', err)
-        }
-
-        console.log("added 'STAFF' to roles collection")
-      })
+  await Role.deleteMany({}, (err) => {
+    if (err) {
+      console.log('Unable to delete roles!')
+      return
     }
+    Role.estimatedDocumentCount((err, count) => {
+      if (!err && count === 0) {
+        new Role({
+          name: 'SUPERADMIN'
+        }).save((err) => {
+          if (err) {
+            console.log('error', err)
+          }
+
+          console.log("added 'SUPERADMIN' to roles collection")
+        })
+
+        new Role({
+          name: 'ADMIN'
+        }).save((err) => {
+          if (err) {
+            console.log('error', err)
+          }
+
+          console.log("added 'ADMIN' to roles collection")
+        })
+
+        new Role({
+          name: 'USER'
+        }).save((err) => {
+          if (err) {
+            console.log('error', err)
+          }
+
+          console.log("added 'USER' to roles collection")
+        })
+
+        new Role({
+          name: 'MODERATOR'
+        }).save((err) => {
+          if (err) {
+            console.log('error', err)
+          }
+
+          console.log("added 'MODERATOR' to roles collection")
+        })
+
+        new Role({
+          name: 'STAFF'
+        }).save((err) => {
+          if (err) {
+            console.log('error', err)
+          }
+
+          console.log("added 'STAFF' to roles collection")
+        })
+      }
+    })
   })
-})
+
+  await User.deleteMany({}, async function (err) {
+    if (err) {
+      console.log('Unable to delete users!')
+      return
+    }
+
+    const roleData = await Role.findOne({ name: { $in: 'SUPERADMIN' } })
+
+    const user = new User({
+      firstName: 'Super',
+      lastName: 'Admin',
+      username: 'superadmin',
+      email: 'superadmin@mailinator.com',
+      password: '123456',
+      role: roleData._id,
+      active: true
+    })
+
+    user.save(function (err, savedUser) {
+      if (err) {
+        console.log('Unable to create users!')
+        return
+      }
+      console.log('Database has been cleared. Default user has been added. _id: ' + savedUser._id)
+      process.exit(0)
+    })
+  })
+}
+
+// Call Fn to reset the DB
+resetDBFn()
